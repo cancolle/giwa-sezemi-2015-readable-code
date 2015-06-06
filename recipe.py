776b77745cc1a3286105f9c3166fbf597273f3fb
giwa <ugw.gi.world@gmail.com>
2015-06-06 11:05:31 +0900

    spec1

---
+++ recipe.py
@@ -0,0 +1,27 @@
+class Recipe:
+    """
+    Recipe class for sezemi 2015
+    """
+    def _init__(self):
+        pass
+
+    def test(self):
+        """
+        Task1 print a dish
+
+        >>> recipe = Recipe()
+        >>> recipe.test()
+        オムライス
+        """
+        print("オムライス")
+
+
+def _test():
+    import doctest
+    (failure_count, test_count) = doctest.testmod(optionflags=doctest.ELLIPSIS)
+    if failure_count:
+        exit(-1)
+
+
+if __name__ == "__main__":
+    _test()
